6643b0f474a7432ee4dae97db4ddc9d61f4c9753
biyuehuang <31006098+biyuehuang@users.noreply.github.com>
2023-09-02 22:57:37 +0800

    add auto find .xml models

--- test_batchsize.sh
+++ test_batchsize.sh
@@ -18,4 +18,21 @@ func $MODEL
 echo -e "\n\n+++++++++++  resnet-50-pytorch"
 MODEL=resnet-50-pytorch/FP16-INT8/resnet-50-pytorch.xml
 func $MODEL
+###############################################################
+# 指定要搜索的文件夹路径
+folder_path="/path/to/your/folder"
 
+# 使用find命令查找所有.xml文件，并将结果保存到一个变量中
+xml_files=$(find "$folder_path" -type f -name "*.xml")
+
+# 检查是否找到了.xml文件
+if [ -n "$xml_files" ]; then
+    echo "找到以下.xml文件："
+    echo "$xml_files"
+    for file in $xml_files; do
+        echo "$file"
+        func $file
+    done
+else
+    echo "未找到任何.xml文件。"
+fi
